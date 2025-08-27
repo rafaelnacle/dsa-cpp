dc812c327357babb4fb93c8b14c60ff9f608c132
= <rafaelnacle@hotmail.com>
2025-08-27 15:50:41 -0300

    Big O: O(log n)

--- BigO/main.cpp
+++ BigO/main.cpp
@@ -2,13 +2,13 @@
 
 using namespace std;
 
-// Big O: O(1)
-// even if you have n + n + n which technically is O(2) we simplify it to O(1)
-// This is the most efficient Big O possible.
-int addItems(int n) {
-    return n + n + n;
-}
+// Big O: O(log n)
+// Ex: [1,2,3,4,5,6,7,8] -> split in half [1,2,3,4] [5,6,7,8]
+// check if the value is on the left, if not -> split the half in half again [1,2] [3,4]
+// do it again, if not on the left, split it again [1] [2] -> check which one is the result
+// this would be 2³ = 8 its the same as saying log₂8 = 3
+// or just saying how many times I have to divide the number 8 by 2 to get down to one item.
+// O(log n) is very efficient, much more than O(n), O(n²) and O(nlog n)
 
 int main() {
-    addItems(10);
 }
