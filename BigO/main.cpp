d9a25a774a74f74771cb0b2cd58bf314b59d035e
= <rafaelnacle@hotmail.com>
2025-08-27 15:24:31 -0300

    Big O: Drop Non-Dominants

--- BigO/main.cpp
+++ BigO/main.cpp
@@ -2,13 +2,21 @@
 
 using namespace std;
 
-// n * n = n² -> O(n²)
-// It is a lot less efficient than O(n)
+// Big O: Drop Non-Dominants
+// O(n² + n)
+// n² -> dominant term
+// n -> non dominant term (we drop the n because its insignificant)
 void printItems(int n) {
+    // O(n²)
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             cout << i << j << endl;
         }
+    }
+
+    // O(n)
+    for (int k = 0; k < n; k++) {
+        cout << k << endl;
     }
 }
 
