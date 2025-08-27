436c682a35e5e00ee8813ef5b76b4717e55c1394
= <rafaelnacle@hotmail.com>
2025-08-27 15:21:20 -0300

    O(n²) example

--- BigO/main.cpp
+++ BigO/main.cpp
@@ -2,13 +2,13 @@
 
 using namespace std;
 
+// n * n = n² -> O(n²)
+// It is a lot less efficient than O(n)
 void printItems(int n) {
     for (int i = 0; i < n; i++) {
-        cout << i << endl;
-    }
-
-    for (int j = 0; j < n; j++) {
-        cout << j << endl;
+        for (int j = 0; j < n; j++) {
+            cout << i << j << endl;
+        }
     }
 }
 
